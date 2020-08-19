26c1aafa0ebc5b62e9a746a49fe7bf274b1ac547
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 21:42:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length 
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    
+    couponed_item_name
   
     counter += 1 
   end 
