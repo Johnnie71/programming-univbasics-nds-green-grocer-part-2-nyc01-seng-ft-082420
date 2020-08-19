5adb069c80ae4a3cab7eb2031ad766e774e15302
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 22:43:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,6 +41,6 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
-  clearanced_cart= apply_clearance(coup)
+  clearanced_cart= apply_clearance(couponed_cart)
   
 end
