8791371eb32caa6b350d7b05aecb130e7075cda6
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 21:56:55 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -11,6 +11,9 @@ def apply_coupons(cart, coupons)
          cart_item_with_coupon[:count] += coupons[counter][:num]
          cart_item[count] -= coupons[counter][:num]
       else 
+        cart_item_with_coupon = {
+          
+        }
       
     end 
     counter += 1 
