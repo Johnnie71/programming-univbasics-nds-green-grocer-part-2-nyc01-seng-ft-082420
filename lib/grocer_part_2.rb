466323d5029df9e6c99d6e52ec6f1f4b2526dcce
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 21:40:54 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,9 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
   counter = 0 
   while counter < coupons.length 
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
   
     counter += 1 
   end 
