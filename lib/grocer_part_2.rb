03ed8c3fc450f98b50237b03a708e9ccd9c5f5e8
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-19 22:31:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -32,6 +32,7 @@ def apply_clearance(cart)
   while counter < cart.length
     if cart[counter][:clearance]
       cart[counter][:price] = cart[counter][:price] - (cart[counter][:price] * 0.2)
+      
     end 
   end
 end
